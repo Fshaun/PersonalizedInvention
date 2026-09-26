c75eca767cbbc74a57ff273e6eae4bf0c67dcbb8
fhulufheloshaun97 <fhulufheloshaun97@gmail.com>
2026-09-27 00:10:49 +0200

    Prod Evnvironment

---
+++ personalized-invention/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://personalized-invention-api.onrender.com',
+  stripePublishableKey: 'pk_test_51UHR6ZAAzPr9yFNXkq7tcjHAZR8Zh8wro4faf10WwDQZ3UeoYZizX1xL8DWahpjv6vZ5V6Deb1Rowqn56lWYImf400f6BvHYya'
+};
